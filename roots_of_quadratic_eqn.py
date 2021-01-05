63bded600f8c4f2a950ed18ce2c6deb88b25edbf
samuel jazz john <samueljazzjohn@gmail.com>
2021-01-05 10:34:45 +0530

    new condition added in quadratic equation programme

--- roots_of_quadratic_eqn.py
+++ roots_of_quadratic_eqn.py
@@ -4,7 +4,9 @@ a = int(input("Enter coefficients of quadratic equation:"))
 b = int(input())
 c = int(input())
 d = (b * b) - (4 * a * c)
-if d >= 0:
+if a==0:
+    print("It is not a quadratic equation")
+elif d >= 0:
     r1 = (-b + math.sqrt(d)) / 2
     r2 = (-b - math.sqrt(d)) / 2
 else:

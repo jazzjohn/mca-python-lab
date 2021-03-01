151503649334aceef76199399f3545ece5ccf000
samueljazzjohn <jazzjohyn1999@gmail.com>
2021-03-01 22:49:51 +0530

    filehandling modified

--- read_csv.py
+++ read_csv.py
@@ -1,5 +1,6 @@
 import csv
-string=[]
+lst=[]
+str=()
 with open("city.csv","w") as file:
     write=csv.writer(file)
     write.writerow(["achu","sachu","sanu"])
@@ -7,5 +8,7 @@ with open("city.csv","r") as file:
     read=csv.reader(file)
     for row in read:
         print(row)
-        string=.join()
+        lst.append(",".join(row))
+
+print(type(lst[0]))
 

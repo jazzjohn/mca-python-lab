b2eba00006c01f3f23586ff1ffe28afe430e346a
samueljazzjohn <jazzjohyn1999@gmail.com>
2021-03-01 23:03:19 +0530

    modified csv

--- read_csv.py
+++ read_csv.py
@@ -3,12 +3,12 @@ lst=[]
 str=()
 with open("city.csv","w") as file:
     write=csv.writer(file)
-    write.writerow(["achu","sachu","sanu"])
+    write.writerows(["achu","sachu","sanu"])
 with open("city.csv","r") as file:
     read=csv.reader(file)
     for row in read:
         print(row)
-        lst.append(",".join(row))
+        lst.append("".join(row))
 
 print(type(lst[0]))
 
